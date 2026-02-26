2f4f3cc418d196ffc84975b96ccabc3c45b290dc
radhakantarout <radhakanta.rout16@gmail.com>
2026-02-27 09:40:45 +1100

    add login and signup2

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { confirmUser } from "@/lib/cognito";
